3cd4dcf729006bfdbc25394016b78a80623e05aa
bobik <104901009+alecsbobik@users.noreply.github.com>
2024-03-06 21:15:22 +0800

    This is my second commit of changes adding the blog code

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
+
 const express = require('express');
+
+// Added
+const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+// 
+
 const mongoose = require('mongoose');
-const Post = require('./server/models/Post');
+// const Post = require('./server/models/Post');
 
 const app = express();
 const PORT = process.env.PORt || 3000;
@@ -16,6 +28,23 @@ const connectDB = async () => {
         process.exit(1);
     }
 }
+
+// Added
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    // cookie: {maxAge new Date (Date.now() + (3600000) )}
+}));
+// 
 
 app.get('/', (req, res) => {
     res.send({ title: 'Posts' });
@@ -47,11 +76,21 @@ app.get('/Post', async (req, res) => {
     }
 })
 
+// Added
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.locals.isActiveRoute = isActiveRoute;
+
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 
-
-
-
+// https://blue-handsome-chameleon.cyclic.app
 
 
 connectDB().then(() => {
